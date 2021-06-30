3208a3c901521984022353ce0b1296ac0058cd7e
Matias Borrazas <borrazas.trabajo@gmail.com>
2021-06-30 20:28:15 -0300

    cambios

--- detail.php
+++ detail.php
@@ -572,11 +572,32 @@ MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a
     // Crea un objeto de preferencia
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
     $preference = new MercadoPago\Preference();
+    $payer = new MercadoPago\Payer();
+    $payer->name = "Charles";
+    $payer->surname = "Luevano";
+    $payer->email = "charles@hotmail.com";
+    $payer->date_created = "2018-06-02T12:58:41.425-04:00";
+    $payer->phone = array(
+        "area_code" => "",
+        "number" => "949 128 866"
+    );
+
+    $payer->identification = array(
+        "type" => "DNI",
+        "number" => "12345678"
+    );
+
+    $payer->address = array(
+        "street_name" => "Cuesta Miguel Armendáriz",
+        "street_number" => 1004,
+        "zip_code" => "11020"
+    );
     $item = new MercadoPago\Item();
     $item->title = 'Mi producto';
     $item->quantity = 1;
     $item->unit_price = 75;
     $preference->items = array($item);
+    $preference->payer = $payer;
     $preference->purpose = 'wallet_purchase';
     $preference->back_urls = array(
         "success" => "https://mborrazas-mp-ecommerce-php.herokuapp.com/success.php",

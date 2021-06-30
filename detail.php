a6d4d2fd74f5495b9356724cddad726f29558f27
Matias Borrazas <borrazas.trabajo@gmail.com>
2021-06-30 20:13:41 -0300

    cambios

--- detail.php
+++ detail.php
@@ -2,7 +2,7 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('TEST-6660607320613134-011301-03d3d1b3159336d6218c7ec7f4acd9f5__LD_LA__-147823178');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 
 ?>
@@ -582,7 +582,7 @@ MercadoPago\SDK::setAccessToken('TEST-6660607320613134-011301-03d3d1b3159336d621
     ?>
     <script src="https://sdk.mercadopago.com/js/v2"></script>
     <script>
-        const mp = new MercadoPago('TEST-110bb6ca-435f-44b2-8f47-46b60f411077', {
+        const mp = new MercadoPago('APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626', {
             locale: 'es-UY'
         });
 

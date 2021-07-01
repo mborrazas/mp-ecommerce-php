9018c2f656791f07a4c35379ba3109bd1f5f6262
Matias Borrazas <borrazas.trabajo@gmail.com>
2021-06-30 23:27:10 -0300

    cambios

--- notifications.php
+++ notifications.php
@@ -4,7 +4,9 @@ require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-error_log(json_encode($_POST));
+$data = json_decode(file_get_contents('php://input'), true);
+
+error_log(json_encode($data));
 $payment = [];  
 
 $value = $_GET["topic"] ?? $_GET['type'];
